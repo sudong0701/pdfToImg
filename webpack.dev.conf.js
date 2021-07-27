001ce0e461b988a0b5f3abb1c502d696166391f2
sudong.duan <1006364428@qq.com>
2021-07-27 16:24:58 +0800

    <feat>:配置优化

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -10,7 +10,7 @@ module.exports = {
 		publicPath: '',
 		library: 'laputarenderer',
 		libraryTarget: 'umd',
-        libraryExport: 'default'
+        //libraryExport: 'default'
 	},
 	devtool: 'none',
 	mode: 'development',   //
